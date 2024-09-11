893a6a98fceb715cb2c06f7384d58fc379fe0b66
Praveen <praveen.k@xnara.com>
2024-09-11 08:42:43 +0530

    added github command

--- tests/annotations.spec.js
+++ tests/annotations.spec.js
@@ -66,5 +66,28 @@ test("Slow",async({page})=>{
 // git checkout -b <branchname> Ex;- "sept09"
 
  
+//Clone the repository
 
+// git clone https://github.com/Praveenroy05/PlaywrightAug24.git - clone the repository in local system
+
+// git status  - This will provide all the latest changes made in the local system
+
+// git branch - This will return us the current branch that we are on in the local system
+
+// git checkout -b <branch name> EX: - git checkout -b "newbeanch"
+
+// git checkout master - moving from one branch to another
+
+// git pull - To pull all the latest code/changes from remote git to local
+
+
+/**
+ * 1. To check the status - git status
+ * 2. To add all the changes to the local git first - git add .
+ * 3. To commit all the changes  - git commit -m "added login page test script"
+ * 
+ * 
+ * 
+ * 
+ */
 
